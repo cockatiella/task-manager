6811e86d0ee6c34fdefbeb1eeca3f45ee4b6bc5b
Sofia Dinatolo <92044969+cockatiella@users.noreply.github.com>
2022-11-24 15:22:41 +0100

    changes

--- middleware/async.js
+++ middleware/async.js
@@ -7,5 +7,6 @@ const asyncWrapper = (fn) => {
       }
     }
   }
-  
+  //this func can be used as a middleware for any routes. it wraps the function  that is passed to a try catch block.
+  //if there is an error, it will be passed to the next middleware which is the error handler.
   module.exports = asyncWrapper
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -6,4 +6,6 @@ const errorHandlerMiddleware = (err, req, res, next) => {
   return res.status(500).json({ msg: 'Something went wrong, please try again' })
 }
 
+//we create a middleware func that accepts the error obj as param and checks if the err is an instance of the customapierror class. if it is, return a res with status code and err mess. if not we return status 500 and generic err message:
+
 module.exports = errorHandlerMiddleware
